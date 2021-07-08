6811e3b7bef01b5e1f2f2db3d85cc90ed7d92f35
wise28 <wise28@yandex.ru>
2021-07-08 13:07:00 +0300

    semantic mockup

---
+++ 03-lection1/01-button/main.js
@@ -0,0 +1,37 @@
+let body = document.body;
+
+function disableBtn(btn) {
+    btn.disabled = true;
+}
+
+function enableBtn(btn) {
+    btn.disabled = false;
+}
+
+let button_primary = document.body.querySelector('.btn-primary');
+body.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target == button_primary) {
+        disableBtn(button_primary);
+    }else {
+        enableBtn(button_primary);
+    }
+} ) 
+let button_secondary = document.body.querySelector('.btn-secondary');
+body.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target == button_secondary) {
+        disableBtn(button_secondary);
+    }else {
+        enableBtn(button_secondary);
+    }
+} ) 
+let button_alternate = document.body.querySelector('.btn-alternate');
+body.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target == button_alternate) {
+        disableBtn(button_alternate);
+    }else {
+        enableBtn(button_alternate);
+    }
+} ) 
